f2365d51f6a9792bc34c05fdc032167d52abe487
felipegdm <felipegmelo@usp.br>
2019-05-13 10:15:59 -0300

    More PEP8 changes

--- tarefa_2.py
+++ tarefa_2.py
@@ -6,16 +6,16 @@ from main import *
 import numpy as np
 
 A = np.array([[3/10, 3/5, 0],
-               [1/2,   0, 1],
+              [1/2,   0, 1],
               [4/10, 4/5, 0]])
 
 W = np.array([[3/5, 0],
-                [0, 1],
+              [0, 1],
               [4/5, 0]])
 
 H = np.array([[1/2, 1, 0],
               [1/2, 0, 1]])
-                
+
 np.set_printoptions(precision=3, suppress=True)
 
 P, Q = resolve_mmq(A, W)
