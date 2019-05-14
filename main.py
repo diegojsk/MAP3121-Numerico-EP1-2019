db0fabba4336241f4c682ee65c4752d5647679eb
felipegdm <felipegmelo@usp.br>
2019-05-14 13:39:07 -0300

    Improve performace and time measurement

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import numpy as np
 import math
+import time
 from cmath import sqrt
 
 ERR = 1e-4
 MAX_ITER = 1e2
 SUPPORTED_FORMATS = [np.float32, np.float64, np.float_,
                      np.complex64, np.complex128, np.complex_]
+
 
 def calc_c(a, b):
     """
@@ -28,11 +30,10 @@ def calc_c(a, b):
 
     if abs(a) > abs(b):
         T = -np.divide(b, a)
-        cos = 1/np.sqrt(1+(T**2))
+        return np.divide(1, np.sqrt(1 + np.power(T, 2)))
     else:
         T = -np.divide(a, b)
-        cos = calc_s(a, b)*T
-    return cos
+        return np.divide(T, np.sqrt(1 + np.power(T, 2)))
 
 
 def calc_s(a, b):
@@ -56,11 +57,10 @@ def calc_s(a, b):
 
     if abs(a) > abs(b):
         T = -np.divide(b, a)
-        sen = calc_c(a, b)*T
+        return np.divide(T, np.sqrt(1 + np.power(T, 2)))
     else:
         T = -np.divide(a, b)
-        sen = 1/np.sqrt(1+(T**2))
-    return sen
+        return np.divide(1, np.sqrt(1 + np.power(T, 2)))
 
 
 def rot_givens(W, n, m, i, j, c, s):
@@ -82,15 +82,19 @@ def rot_givens(W, n, m, i, j, c, s):
             \nNote que tipos inteiros levam a perdas severas \
             por arredondamento.".format(W.dtype))
 
-    n, m = W.shape
-    col = 0
-    while (col < m) and (W[i][col] == 0 and W[j][col] == 0):
-        col += 1
+    aux = c*W[i, :] - s*W[j, :]
+    W[j, :] = s*W[i, :] + c*W[j, :]
+    W[i, :] = aux
 
-    for r in range(col, m):
-        aux = c*W[i][r] - s*W[j][r]
-        W[j][r] = s*W[i][r] + c*W[j][r]
-        W[i][r] = aux
+    # for r in range(col, m):
+    # n, m = W.shape
+    # col = 0
+    # while (col < m) and (W[i][col] == 0 and W[j][col] == 0):
+    #     col += 1
+
+    #     aux = c*W[i][r] - s*W[j][r]
+    #     W[j][r] = s*W[i][r] + c*W[j][r]
+    #     W[i][r] = aux
 
 
 def zera_elemento(W, i, j, k):
@@ -156,7 +160,7 @@ def resolver_sist(W, A):
     """
     n1, p = W.shape
     n2, m = A.shape
-    n = None
+    n = n1
 
     if n1 != n2:
         raise ValueError("Matrizes de tamanhos incompatíveis!")
@@ -274,8 +278,8 @@ def resolve_mmq(A, W0):
     err = residuo(_A, W0, H)
     prev_err = err + 1
 
-    while (np.power(err - prev_err, 2) > np.power(ERR, 2)) and (i < MAX_ITER):
-
+    while (np.abs(err - prev_err) > ERR) and (i < MAX_ITER):
+        begin = time.time()
         i += 1
         normaliza(W)
         print("- Solving for H")
@@ -301,8 +305,9 @@ def resolve_mmq(A, W0):
 
         prev_err = err
         err = residuo(A, W, H)
-
-        print("=> Iteration {0} Abs Error {1:.3f} Delts {2:.3f}".format(i, err, prev_err-err))
+        end = time.time()
+        print("=> Iteration {0} Abs Error {1:.3f} Delta {2:.3f} Time {3:.3f}"
+              .format(i, err, prev_err-err, end - begin))
 
     return (W, H)
 
@@ -328,14 +333,16 @@ def treinamento(d, p=10, ndig_treino=100):
     Executar treinamento do dígito d gerando a matriz Wd
     '''
 
+    folder = "{0}-{1}".format(ndig_treino, p)
+
     A = matriz_arquivo('dados_mnist/train_dig'+str(d)+'.txt', ndig_treino)
     print("Matrix loaded!")
     n, m = A.shape
     W = np.random.rand(n, p)
     Wd, H = resolve_mmq(A, W)
 
-    np.save('./treinamento/W'+str(d)+'.npy', Wd)
-    np.save('./treinamento/H'+str(d)+'.npy', H)
+    np.save('./{0}/W{1}.npy'.format(folder, d), Wd)
+    np.save('./{0}/H{1}.npy'.format(folder, d), H)
 
     return Wd
 
--- treinamentos.py
+++ treinamentos.py
@@ -5,10 +5,10 @@ import numpy as np
 import time
 
 begin = time.time()
-for d in range(10):
+for d in range(5, 10):
     partial = time.time()
-    print("Loading file ({}/10)".format(d+1))
-    treinamento(d)
+    print("[LOG] Loading file ({}/10)".format(d+1))
+    treinamento(d, p=10, ndig_treino=1000)
     end = time.time()
-    print("Finished file {0} in {1} s".format(d+1, end - partial))
-    print("Total elapsed time {0} in {1} s".format(d+1, end - partial))
+    print("[LOG] Finished file {0} in {1} s".format(d+1, end - partial))
+    print("[LOG] Total elapsed time {0} in {1} s".format(d+1, end - begin))
