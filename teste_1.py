c38ce40874a09005e86874db990c02970d9cd2b8
felipegdm <felipegmelo@usp.br>
2019-05-21 21:21:34 -0300

    Small adjusts on teste_*

--- teste_1.py
+++ teste_1.py
@@ -1,19 +1,19 @@
 from main import fatorar_qr
 import numpy as np
 
-A = np.array([ [1, 2, 3, 4],
-               [2, 3, 4, 5],
-               [3, 4, 5, 6],
-               [4, 5, 6, 7] ]).astype(np.double)
+A = np.array([[1, 2, 3, 4],
+              [2, 3, 4, 5],
+              [3, 4, 5, 6],
+              [4, 5, 6, 7]]).astype(np.double)
 
-B = np.array([ [1, -2, 3, 4],
-               [-2, -3, 4, 5],
-               [3, 4, -5, -6],
-               [1, -9, 7, -10]]).astype(np.double)
+B = np.array([[1, -2, 3, 4],
+              [-2, -3, 4, 5],
+              [3, 4, -5, -6],
+              [1, -9, 7, -10]]).astype(np.double)
 
 np.set_printoptions(precision=3, suppress=True)
 
-print("{0:.1f}".format(np.linalg.det(A)) )
+print("{0:.1f}".format(np.linalg.det(A)))
 # 0.0
 print("{0:.1f}".format(np.linalg.det(B)))
 # 30.0
--- teste_2.py
+++ teste_2.py
@@ -2,11 +2,11 @@ from main import resolver_sist
 import numpy as np
 
 A = np.array([[3/10, 3/5, 0],
-               [1/2,   0, 1],
+              [1/2,   0, 1],
               [4/10, 4/5, 0]])
 
 W = np.array([[3/5, 0],
-                [0, 1],
+              [0,   1],
               [4/5, 0]])
 
 H = np.array([[1/2, 1, 0],
